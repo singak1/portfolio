9368e3493a720f3f0046421817c3b731192f4327
singak1 <a97sin@gmail.com>
2023-07-11 19:59:16 -0400

    Update button toggle for darkmode

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom"
 import { Tooltip, IconButton, Flex, Heading, Spacer, useColorMode } from "@chakra-ui/react"
-import { CgDarkMode } from "react-icons/cg"
+import { BsSunFill, BsMoonFill } from "react-icons/bs"
 
 const Navbar = () => {
-    const { toggleColorMode } = useColorMode()
+    const { colorMode, toggleColorMode } = useColorMode()
     return (
         <Flex minWidth='max-content' alignItems='center' gap='2' padding='4' borderBottom='2px' borderBottomColor='GrayText'>
             <Link to="/"><Heading as='h4' size='md' mr="4">Home</Heading></Link>
             <Link to="/contact"><Heading as='h4' size='md'>Contact</Heading></Link>
             <Spacer />
-            <Tooltip label='Toggle Dark Mode'><IconButton onClick={toggleColorMode}
-                borderRadius='30'
-                fontSize='30'
-                icon={<CgDarkMode />}
-            /></Tooltip>
+            <Tooltip label='Toggle Dark Mode'>
+                <IconButton onClick={toggleColorMode}
+                    borderRadius='30'
+                    fontSize='20'
+                    icon={colorMode === "light" ? <BsMoonFill /> : <BsSunFill />}
+                />
+            </Tooltip>
         </Flex>
     )
 }
